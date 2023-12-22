92dd1624bdc42414457336acfbf6eeaa2f96da72
abdallhfattah <abdallh.fattah@33gmail.com>
2023-12-22 20:38:19 +0200

    main stucture for the project

---
+++ order-and-notification/src/main/java/com/orderandnotification/orderandnotification/OrderAndNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.orderandnotification.orderandnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAndNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAndNotificationApplication.class, args);
+	}
+
+}
---
+++ order-and-notification/src/test/java/com/orderandnotification/orderandnotification/OrderAndNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderandnotification.orderandnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderAndNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
