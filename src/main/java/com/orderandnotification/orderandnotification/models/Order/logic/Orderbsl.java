0a761653a04881599aba644418eebe57c3a43be1
georgemarie <102537518+georgemarie@users.noreply.github.com>
2023-12-30 14:11:14 +0200

    fixing errors

--- src/main/java/com/orderandnotification/orderandnotification/models/Order/logic/Orderbsl.java
+++
@@ -1,8 +0,0 @@
-package com.orderandnotification.orderandnotification.models.Order.logic;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class Orderbsl {
-	
-}
--- src/main/java/com/orderandnotification/orderandnotification/models/Shippment/IShippingStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.orderandnotification.orderandnotification.models.Shippment;
-
-import com.orderandnotification.orderandnotification.models.Order.Order;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface IShippingStrategy {
-    public void ship(Order order);
-}
